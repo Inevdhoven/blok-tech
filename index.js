bb1113ad506ac1a78f8b5318ddc58b6947a5725e
Mona Lisa <mona@lisa.com>
2022-02-10 16:04:09 +0100

    Huiswerk week 1 Backend 10/02/2022 16:04

---
+++ index.js
@@ -0,0 +1,12 @@
+const camelCase = require('camelcase');
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send("hello world");
+})
+
+app.listen(3000)
+
+
+console.log(camelCase('foo-bar'));
