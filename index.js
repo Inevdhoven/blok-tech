442beaa4f368f662613c03818a2a9f95eb975ea2
Mona Lisa <mona@lisa.com>
2022-02-25 13:53:42 +0100

    Installed handlebars and added a license

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const camelCase = require('camelcase');
 const express = require('express');
 const app = express();
+const { engine } = require('express-handlebars')
+const PORT = 3000;
 
-app.get('/', function(req, res){
-    res.send("hello world");
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use('/static', express.static('static'));
+
+app.get('/', onHome);
+app.get('/signup', onSignUp)
+app.get('/login', onLogIn)
+
+function onHome(req, res) {
+    res.render('home');
+}
+
+function onSignUp(req, res) {
+    res.render('signup');
+}
+
+function onLogIn(req, res) {
+    res.render('login');
+}
+
+app.use((req, res, next) => {
+    res.status(404).send('404 page not found');
 })
 
-app.listen(3000)
-
-
-console.log(camelCase('foo-bar'));
-console.log(camelCase('ine-van-den-hoven'));
+app.listen(PORT, () => {
+    console.log('app running on port', PORT);
+})
