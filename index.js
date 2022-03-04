950f19ca63ca084fa5c012abf558cf2b6d4812d6
InevdHoven <inevdhoven@gmail.com>
2022-03-04 10:42:04 +0100

    :white_check_mark: Parser en Multer toegevoegd

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const { engine } = require('express-handlebars')
+const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+const multer  = require('multer');
+const upload = multer({ dest: 'static/uploads/' });
+const myData = require('./data/data.json');
 const PORT = 3000;
 
 app.engine('handlebars', engine());
@@ -9,27 +13,30 @@ app.set('views', './views');
 
 app.use('/static', express.static('static'));
 
-app.get('/', onHome);
-app.get('/signup', onSignUp)
-app.get('/login', onLogIn)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-function onHome(req, res) {
-    res.render('home', {
-        post: {
-            author: 'Ine van den Hoven',
-            age: '22',
-            comments: []
-        }
-    });
-}
+app.post('/accountaangemaakt',(req, res)=> {
+    try {
+        // database logic schrijven..
+        res.send('Naam: ' +req.body.user +' en wachtwoord: ' +req.body.password);   
+    } catch (error) {
+        throw new Error(error)
+    }
+});
 
-function onSignUp(req, res) {
-    res.render('signup');
-}
+app.get('/', (req, res) => {
+    res.render('home', myData);
+});
 
-function onLogIn(req, res) {
-    res.render('login');
-}
+app.get('/signup', (req, res) => {
+    res.render('signup', { title: 'Signup - BookBuddy'});
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login - BookBuddy'});
+});
 
 app.use((req, res, next) => {
     res.status(404).send('404 page not found');
