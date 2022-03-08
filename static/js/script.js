066a183a35ff30a98c1cc6d461ed8751d6a8902e
InevdHoven <inevdhoven@gmail.com>
2022-03-08 13:42:54 +0100

    :sparkles: Nieuwe map voor JS features

---
+++ static/js/script.js
@@ -0,0 +1,33 @@
+
+// const form = document.querySelector('form');
+// const submit = document.querySelector('#login');
+// const email = document.querySelector('#email');
+// const password = document.querySelector('#password');
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+
+// });
+
+//Signup page
+// const form = document.querySelector('form');
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     const signupPass = document.querySelector('#password').value;
+//     const signupConfirmPass = document.querySelector('#confirm_password').value;
+
+//     if (signupPass.length != 0) {
+//         if (signupPass == signupConfirmPass) {
+//             console.log("je wachwoord is hetzelfde");
+//         } else {
+//             console.log("je wachtwoord is niet gelijk");
+//         }
+//     } else {
+//         console.log("wachtwoord mag niet leeg zijn")
+//     }
+// });
+
+// console.log("test");
