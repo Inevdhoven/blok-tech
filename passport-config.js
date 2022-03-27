a101fc97c4f2c307e9707edb281baa63e32c02fd
InevdHoven <inevdhoven@gmail.com>
2022-03-27 11:37:44 +0200

    :sparkles: Passport toegevoegd

---
+++ passport-config.js
@@ -0,0 +1,39 @@
+// Source: https://github.com/WebDevSimplified/Nodejs-Passport-Login
+const localStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const User = require('./models/User');
+
+module.exports = function (passport) {
+
+    passport.use(
+        new localStrategy({ usernameField: 'email' }, (email, password, done) => {
+            User.findOne({
+                email: email
+            }).then(async user => {
+                if (!user) {
+                    console.log('email not found')
+                    return done(null, false, { message: 'Het emailadres dat is ingevuld is niet geregistreerd' });
+                }
+
+                const match = await bcrypt.compare(password, user.password);
+                
+                if (match) {
+                    return done(null, user);
+                } else {
+                    console.log('password incorrect');
+                    return done(null, false, { message: 'Het wachtwoord dat is ingevuld is niet correct' });
+                }
+            });
+        })
+    );
+
+    passport.serializeUser((user, done) =>{
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) =>{
+        User.findById(id,  (err, user) =>{
+            done(err, user);
+        });
+    })
+};
